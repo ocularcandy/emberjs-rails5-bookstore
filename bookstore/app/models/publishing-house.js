4739d72c0e2659f9a7f2040c81aae030867b32fb
Amber Hayes <amber@ocularcandy.biz>
2017-02-20 22:04:00 -0600

    initial commit emberjs-rails5 bookstore application

---
+++ bookstore/app/models/publishing-house.js
@@ -0,0 +1,11 @@
+import Publisher from './publisher';
+
+export default Publisher.extend();
+// OG generated code
+// import DS from 'ember-data';
+//
+// export default DS.Model.extend({
+//   name: DS.attr('string'),
+//   discount: DS.attr('number'),
+//   books: DS.hasMany('book')
+// });
